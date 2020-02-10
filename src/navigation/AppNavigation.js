29ea3bfc6fba0b1ad694f96e77339f2cdda023d7
saadelsabahy <36341653+saadelsabahy@users.noreply.github.com>
2020-02-10 19:52:49 +0200

    delte unusable imports and library

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from '../screens/Home/HomeScreen';
 import Search from '../screens/Search';
+
 const AppNavigation = () => {
    const Stack = createStackNavigator();
    return (
